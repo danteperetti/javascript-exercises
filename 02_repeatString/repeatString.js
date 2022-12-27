2db39aeb044cf32f30d1b5f7246a77149471b5eb
Dante Peretti <danteperetti34@gmail.com>
2022-12-27 15:09:53 -0500

    Added paramters, if statement, and for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0) {
+        let loopString = string;
+        for(let i = 1; i < num; i++) {
+            loopString += string;
+        }
+        return loopString;
+    } else if (num < 0){
+        return 'ERROR';
+    } else {
+        return '';
+    }
 };
 
 // Do not edit below this line
