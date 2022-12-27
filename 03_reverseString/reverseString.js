d51bcea062d72b6fd3e9973d2cc30ee7f1c56c62
Dante Peretti <danteperetti34@gmail.com>
2022-12-27 15:10:50 -0500

    Added parameter, variables, and for of loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reverseArray = stringArray.reverse();
+    let newString = '';
+    for (char of reverseArray) {
+        newString += [char];
+    }
+    return newString;
 };
 
 // Do not edit below this line
