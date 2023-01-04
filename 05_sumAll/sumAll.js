880e2b002ad3bb595e7b5f54b7dc1fbfbce67043
Dante Peretti <danteperetti34@gmail.com>
2023-01-03 21:28:41 -0500

    Added arguments for small and big number and wrote for loop to sum up the small number to the big number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let finalSum = 0;
+    if (firstInt < 0 || secondInt < 0) {
+        return 'ERROR';
+    } else if (typeof firstInt !== 'number' || typeof secondInt !== 'number') {
+        return 'ERROR'; 
+    } else if (firstInt < secondInt) {
+        for (let i = firstInt; i <= secondInt; i++) {
+            finalSum += i;
+        }
+    } else if (firstInt > secondInt) {
+        for (let i = secondInt; i <= firstInt; i++) {
+            finalSum += i;
+        }
+    } 
+    return finalSum;
 };
 
 // Do not edit below this line
