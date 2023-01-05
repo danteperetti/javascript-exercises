d9921b4d7bb4457595a86f35d4078699c62484ef
Dante Peretti <danteperetti34@gmail.com>
2023-01-05 13:36:03 -0500

    Completed each function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let finalSum = 0;
+  for (value of array) {
+    finalSum += value;
+  }
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    for (let i = number - 1; i >= 1; i--) {
+      number *= i;
+    }
+  }
+  return number;
 };
 
 // Do not edit below this line
