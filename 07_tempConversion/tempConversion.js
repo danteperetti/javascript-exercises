5433f415248859476a973afa6d3d0d654667ddb9
Dante Peretti <danteperetti34@gmail.com>
2023-01-04 21:11:01 -0500

    Added arguments to functions and included formulas to convert degrees and round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const conversion = 5 / 9 * (temp - 32);
+  return Math.round(conversion * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const conversion = temp * (9 / 5) + 32;
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
