51040b9b4e6f3a448730e8fba86ca4151e60a887
Dante Peretti <danteperetti34@gmail.com>
2023-01-07 09:44:16 -0500

    Included variables, an if statement, and a function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").toLowerCase();
+    const noSpaces = removeSpaces(cleanString);
+    const reversedString = noSpaces.split('').reverse().join('');
+    
+    if(noSpaces === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 
+    function removeSpaces(string) {
+        let stringArray = string.split('');
+        let filtered = stringArray.filter(string => string !== ' ');
+        return filtered.join('');
+    }
 };
 
 // Do not edit below this line
