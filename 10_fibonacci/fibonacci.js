f383871c871ba1448ad665126b992fe3a9e166f4
Dante Peretti <danteperetti34@gmail.com>
2023-01-10 12:31:49 -0500

    Added variables, an if statement, and a for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let fibNumber = parseInt(input), highNum = 1, lowNum = 0, tempNum;
+    if (fibNumber < 0) {
+        return 'OOPS';
+    } else {
+        for (;fibNumber > 0; fibNumber--) {
+            tempNum = highNum;
+            highNum = highNum + lowNum;
+            lowNum = tempNum;
+        }
+        return lowNum;
+    }
 };
 
 // Do not edit below this line
