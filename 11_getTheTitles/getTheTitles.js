76e5f359699284b9e5a629671f3171b771452d86
Dante Peretti <danteperetti34@gmail.com>
2023-01-10 15:48:16 -0500

    Added a variable to hold the book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titleArray = Array.from(books, (Object) => Object.title);
+    return titleArray;
 };
 
 // Do not edit below this line
